b5c4a09d7884968c9799cb7fe35e755319ecd5e5
kyu-h <tigerk9212@gmail.com>
2018-07-26 07:17:11 -0700

    sql injection test

---
+++ dapp/send.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+
+<html>
+    <body>
+        <form method="GET" action="test.php">
+            degree = <input type = "text" name = "degree"> <br>
+            <input type = "submit" name = "submit" value = "SUBMIT">
+        </form>
+    </body>
+</html>
